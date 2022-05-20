a526f88a1fa4726f39cee7f81f7682840b73f99c
M-Petrov <95579161+M-Petrov@users.noreply.github.com>
2022-05-20 08:30:34 +0300

    Update Fruit.js

--- Fruit.js
+++ Fruit.js
@@ -1,6 +1,6 @@
 function fruit(fruit,weightGR,pricePerKG){
     let price=(weightGR/1000)*pricePerKG;
     let kg=weightGR/1000;
-    console.log(`I need $${price} to buy ${kg} kilograms ${fruit}.`)
+    console.log(`I need $${price.fixed(2} to buy ${kg.fixed(2} kilograms ${fruit}.`)
 
 }fruit('orange', 2500, 1.80)
