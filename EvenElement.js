9f5e3251fec4e37fabc59ae5eabf47cc3b02042e
M-Petrov <95579161+M-Petrov@users.noreply.github.com>
2022-05-22 07:19:30 +0300

    Add files via upload

---
+++ EvenElement.js
@@ -0,0 +1,8 @@
+function solve(arr){
+    let result=''
+    for (let index = 0; index < arr.length; index+=2) {
+        result+=arr[index]
+        result+=' '
+        
+    }console.log(result)
+}solve(['20', '30', '40','50', '60'])
